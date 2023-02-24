2ed4a83fd6b1bc8aab5bfc77de43c498418fd8c2
Konstantin <Bunaevkonstantin@bk.ru>
2023-02-24 20:56:10 +0400

    Решение четвертой задачи

---
+++ Task_008/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+string user_imput = Console.ReadLine() ?? "";
+int n = int.Parse(user_imput);
+int count = 1;
+while (count <= n)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine($"{("Четное")} - > {(count)}");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
