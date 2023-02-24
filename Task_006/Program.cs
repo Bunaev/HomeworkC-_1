70e5e7b00d97444a0aa157b7e3dd0172ed94ec6d
Konstantin <Bunaevkonstantin@bk.ru>
2023-02-24 19:50:11 +0400

    Решение третьей задачи

---
+++ Task_006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+string user_imput_1 = Console.ReadLine() ?? "";
+int n = int.Parse(user_imput_1);
+if (n % 2 == 0)
+{
+    Console.WriteLine("Ваше число - четное");
+}
+else
+{
+    Console.WriteLine("Ваше число - нечетное");
+}
