7895d269d0fd5c38bc8f02f1a3f15d7781c2c424
Konstantin <Bunaevkonstantin@bk.ru>
2023-02-24 19:25:32 +0400

    Готовое решение первой задачи

--- Task_002/Program.cs
+++ Task_002/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+string user_imput_1 = Console.ReadLine() ?? "";
+int user_number1 = int .Parse(user_imput_1);
+Console.WriteLine("Введите второе число");
+string user_imput_2 = Console.ReadLine() ?? "";
+int user_number2 = int .Parse(user_imput_2);
+if (user_number1 > user_number2)
+{
+    Console.WriteLine($"{("Максимальное значение")} - > {(user_number1)}");
+    Console.WriteLine($"{("Минимальное значение")} - > {(user_number2)}");
+}
+else
+{
+    Console.WriteLine($"{("Максимальное значение")} - > {(user_number2)}");
+    Console.WriteLine($"{("Минимальное значение")} - > {(user_number1)}");
+}
