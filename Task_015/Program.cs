a72532d536153ee9c1372a909ea7153c71537aa5
Konstantin <Bunaevkonstantin@bk.ru>
2023-02-28 18:55:01 +0400

    Решение 15-й задачи

---
+++ Task_015/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+
+if (num1 >= 1 && num1 <= 7)
+{
+    bool result = (num1 == 6 || num1 == 7);
+
+    if (result)
+    {
+        Console.WriteLine("Ура!Выходные!");
+    }
+    else
+    {
+        Console.WriteLine("Работай, крестьянин. Солнце еще высоко.");
+    }
+}
+else
+{
+    Console.WriteLine("Введено неверное число!");
+}
