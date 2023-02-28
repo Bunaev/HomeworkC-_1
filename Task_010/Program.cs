27aadae5cdc47dbda8e20fd4541ca8aad1d674a1
Konstantin <Bunaevkonstantin@bk.ru>
2023-02-28 17:37:50 +0400

    Решение 10-й задачи

---
+++ Task_010/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+if (num1 < 100 || num1 > 999)
+{
+    Console.WriteLine("Введено неверное число");
+}
+else
+{
+    num1 = num1 / 10;
+    num1 = num1 % 10;
+    Console.WriteLine($"Второе число - {num1}");
+}
