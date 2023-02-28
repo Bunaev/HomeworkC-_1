ca2773e284116be4b80715ff9c8505ca3ccabf07
Konstantin <Bunaevkonstantin@bk.ru>
2023-02-28 18:25:32 +0400

    Решение 13-й задачи

---
+++ Task_013/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+int result = 0;
+int result1 = 0;
+int result3 = 0;
+if (num1 < 10000)
+{
+    if (num1 < 1000)
+    {
+        if (num1 < 100)
+        {
+            Console.WriteLine("Третья цифра в заданном Вами числе отсутсвует");
+        }
+        else
+        {
+            result = num1 % 10;
+            Console.WriteLine(result);
+        }
+    }
+    else
+    {
+        result1 = (num1 / 10) % 10;
+        Console.WriteLine(result1);
+    }
+}
+else
+    {
+        result3 = (num1 / 100) % 10;
+        Console.WriteLine(result3);
+    }
