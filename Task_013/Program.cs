899dd34c655019cf66dc1f3f9eaa46172fcfd302
Konstantin <Bunaevkonstantin@bk.ru>
2023-03-03 19:56:49 +0400

    Исправление ошибки в 13-й задаче

--- Task_013/Program.cs
+++ Task_013/Program.cs
@@ -1,31 +1,16 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите число");
 int num1 = int.Parse(Console.ReadLine() ?? "");
-int result = 0;
-int result1 = 0;
-int result3 = 0;
-if (num1 < 10000)
-{
-    if (num1 < 1000)
+    if (num1 < 100)
     {
-        if (num1 < 100)
-        {
-            Console.WriteLine("Третья цифра в заданном Вами числе отсутсвует");
-        }
-        else
-        {
-            result = num1 % 10;
-            Console.WriteLine(result);
-        }
+        Console.WriteLine("Третья цифра в заданном Вами числе отсутсвует");
     }
     else
     {
-        result1 = (num1 / 10) % 10;
-        Console.WriteLine(result1);
-    }
-}
-else
-    {
-        result3 = (num1 / 100) % 10;
-        Console.WriteLine(result3);
+        while (num1 > 999)
+        {
+        num1 = num1 / 10;
+        }
+    num1 = num1 % 10;
+    Console.WriteLine(num1);
     }
