6b51fc92dbf5250154cf32c96b9b1813556f6ad9
Konstantin <Bunaevkonstantin@bk.ru>
2023-03-03 18:23:36 +0400

    Решение 19-й задачи

---
+++ Task_019/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+int number = ImputNumber("Введите пятизначное число");
+bool x = (number > 9999 && number < 100000);
+    if (x == true)
+    {
+        FindPalindrome(number);
+    }
+    else
+    {
+        Console.WriteLine("Введено неверное число");
+    }
+
+// Описание метода FindPalindrome
+
+static void FindPalindrome(int number)
+{
+int first = 0;
+int last = 0;
+int second = 0;
+int fourth = 0;
+first = (number / 10000) % 10;
+last = number % 10;
+second = (number / 1000) % 10;
+fourth = (number / 10) % 10;
+bool result = (first == last && second == fourth);
+if (result == true)
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+
+// Описание метода ImputNumber
+
+static int ImputNumber(string message)
+{
+    Console.WriteLine(message);
+    int n = int.Parse(Console.ReadLine() ?? "");
+    return n;
+}
