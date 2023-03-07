a64099ace59f04c45ebf5e46329ef39e99e01a5c
Konstantin <Bunaevkonstantin@bk.ru>
2023-03-07 19:44:40 +0400

    Решение 29-й задачи

---
+++ Task_029/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int length = ImputNumber("Введите количество элементов массива", "Введено неверное значение");
+int [] array = new int[length];
+PrintArray(array);
+
+void PrintArray(int [] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите {i}-й элемент массива");
+        array[i] = int.Parse(Console.ReadLine() ?? "");
+    }
+    Console.WriteLine($"[{String.Join(",", array)}]");
+}
+
+int ImputNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 2)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
