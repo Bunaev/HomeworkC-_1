cae1494f46a774bacff57dd0e998e36050e979a6
Konstantin <Bunaevkonstantin@bk.ru>
2023-03-07 17:47:12 +0400

    Решение 25-й задачи

---
+++ Task_025/Program.cs
@@ -0,0 +1,29 @@
+﻿int numberA = ImputNumber("Введите первое число", "Введено неверное значение");
+int numberB = ImputNumber("Введите второе число", "Введено неверное значение");
+DegreeNumber(numberA, numberB);
+
+// Описание метода DegreeNumber
+
+void DegreeNumber(int num1, int num2)
+{
+    int result = 1;
+    for (int count = 1; count <= num2; count++)
+    {
+        result = result * num1;
+    }
+    Console.WriteLine(result);
+}
+
+// Описание метода ImputNumber
+
+int ImputNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
