3320965945e497c9f03cb0ea0204ed94f5a07d8a
Konstantin <Bunaevkonstantin@bk.ru>
2023-03-21 23:52:50 +0400

    Решение 54-й задачи

---
+++ Task_054/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Clear();
+
+int rows = new Random().Next(3, 10);
+int columns = new Random().Next(3, 10);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+MaxToMinInRow(array);
+PrintArray(array);
+
+
+
+// Описание метода GetArray
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+// Описание метода PrintArray
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод для упорядочивания элементов в строке от max до min
+
+void MaxToMinInRow(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i,k] < array[i, k + 1])
+                {
+                    int temp = array[i, k+1];
+                    array[i, k+1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
